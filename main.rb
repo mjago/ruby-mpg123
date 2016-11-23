0bbe5e329d251cc22721c13689c86c8087573e61
mjago <martyn.jago@btinternet.com>
2016-11-23 23:56:35 +0000

    Add get_state()

--- main.rb
+++ main.rb
@@ -1,7 +1,18 @@
 require_relative 'ruby-mpg123'
 
+def get_state state
+  case state
+  when 0 then 'STATE_STOPPED'
+  when 1 then 'STATE_PAUSED'
+  when 2 then 'STATE_PLAYING'
+  else
+    puts "Error: Invalid State"
+    exit 1
+  end
+end
+
 def print_status mpg
-  puts "state: #{mpg.state}, frame: #{mpg.frame}, frames_remaining: #{mpg.frames_remaining}"
+  puts "state: #{get_state(mpg.state)}, frame: #{mpg.frame}, frames_remaining: #{mpg.frames_remaining}"
   puts "time: #{mpg.time}, time_remaining: #{mpg.time_remaining}"
   puts "info: #{mpg.info}, "
 end
@@ -9,7 +20,7 @@ end
 p 'here'
 mpg = Mpg123.new
 sleep 0.2
-puts "state: #{mpg.state}, version: #{mpg.version}"
+puts "state: #{get_state(mpg.state)}, version: #{mpg.version}"
 mpg.load('wave.mp3')
 loop do
   sleep 1
@@ -19,11 +30,11 @@ loop do
   break unless mpg.playing?
 end
 puts "Stopping..."
-puts "state: #{mpg.state}"
+puts "state: #{get_state(mpg.state)}"
 mpg.stop
 loop do
   sleep 1
-  puts "state: #{mpg.state}"
+  puts "state: #{get_state(mpg.state)}"
   break if mpg.stopped?
 end
 mpg = nil
